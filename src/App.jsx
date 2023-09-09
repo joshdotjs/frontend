743d68383717ad3fa65d5762eed89a45b546f171
joshdotjs <joshdotjs@gmail.com>
2023-09-09 10:55:50 -0500

    table

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 
 import './App.css';
 import { Container, Typography, Paper, Box } from '@mui/material';
-import level_up from './assets/level-up.gif'
+import level_up from './assets/level-up.gif';
+import UsersTable from './Basic-Table2';
 
 // ==============================================
 
@@ -19,6 +20,42 @@ const Header = () => {
 // ==============================================
 
 const HomePage = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async function() { 
+      const resp = await fetch('https://postgresql-project-fd4ec6c9caab.herokuapp.com/api/users');
+      const data = await resp.json();
+      console.log('data: ', data);
+      setUsers(data);
+    })();
+  }, []);
+
+  return (
+    <>
+      <Header />
+
+      <Container sx={{ bgcolor: 'deepskyblue'}}>
+        
+        <Link to="/">About</Link>
+
+        <Typography variant="h1"
+          sx={{ my: 4, textAlign: 'center', color: 'primary.main' }}
+          >
+          Level Up
+        </Typography>
+
+        <UsersTable { ...{users} }/>
+
+      </Container>
+    </>
+  );
+};
+
+// ==============================================
+
+const AboutPage = () => {
 
   const [] = useState();
 
@@ -36,37 +73,17 @@ const HomePage = () => {
 
       <Container sx={{ bgcolor: 'deepskyblue'}}>
         
+        <Link to="/">Home</Link>
+
         <Typography variant="h1"
           sx={{ my: 4, textAlign: 'center', color: 'primary.main' }}
           >
           Level Up
         </Typography>
 
-        <Link to="/">About</Link>
-
         <Box>
           <img src={level_up} alt="level up" />
         </Box>
-
-      </Container>
-    </>
-  );
-};
-
-// ==============================================
-
-const AboutPage = () => {
-  return (
-    <>
-      <Header />
-
-      <Container sx={{ bgcolor: 'deepskyblue'}}>
-        
-        <Typography variant="h1"
-          sx={{ my: 4, textAlign: 'center', color: 'primary.main' }}
-          >
-          About Page
-        </Typography>
 
       </Container>
     </>
---
+++ src/Basic-Table2.jsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
+function createData(email, password, is_admin) {
+  return { email, password, is_admin };
+}
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0),
+  createData('Ice cream sandwich', 237, 9.0),
+  createData('Eclair', 262, 16.0),
+  createData('Cupcake', 305, 3.7),
+  createData('Gingerbread', 356, 16.0),
+];
+
+export default function BasicTable({ users }) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Is Admin</TableCell>
+            <TableCell align="right">Password&nbsp;(hashed)</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((row) => (
+            <TableRow
+              key={row.email}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{String(row.is_admin)}</TableCell>
+              <TableCell align="right">{row.password}</TableCell>
+              <TableCell align="right">TODO</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
