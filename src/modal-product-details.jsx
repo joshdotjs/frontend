cadd80ca3d874f24c25173a133c48f0aed474872
joshdotjs <joshdotjs@gmail.com>
2023-10-01 19:31:22 -0500

    remove modal button

--- src/modal-product-details.jsx
+++ src/modal-product-details.jsx
@@ -42,7 +42,6 @@ export default function TransitionsModal({ open, setOpen, product }) {
 
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
