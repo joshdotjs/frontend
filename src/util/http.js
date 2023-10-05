df3c3b1e24b4f1088f9b3f172e2590cb0dad7c96
joshdotjs <joshdotjs@gmail.com>
2023-10-04 19:01:58 -0500

    simplified http.js

--- src/util/http.js
+++ src/util/http.js
@@ -4,62 +4,26 @@ import { getLS } from './local-storage';
 
 const http = async ({ url, method='GET', body={} }) => {
 
+  console.log('http.js');
+
   // let debug_str = `%cmaking REQUEST to ${url} \n- METHOD:  ${method} \n- BODY: ${JSON.stringify(body, null, 2)}`;
   // console.log(debug_str, 'color: orange');
-
-  let resp;
+  
+  let config = { 
+    method, 
+    headers: { 'Content-Type': 'application/json' } 
+  };
+  if (method !== 'GET') // GET requests do not have a body
+    config.body = JSON.stringify( body );
 
   const token = getLS('token');
+  if (token) // auth protected endpoint
+    config.headers.Authorization = token;
+  
+  const resp = await fetch(url, config);
 
-  if (token) { // auth protected endpoint
-    console.log('http.js - if(token)');
-    if (method === 'GET') {
-      resp = await fetch(url, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: token
-        },
-      });
-    }
-    else {
-      resp = await fetch(url, {
-        method,
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: token
-        },
-        body: JSON.stringify( body ),
-      });
-    }
-  } else { // public endpoint
-    if (method === 'GET') {
-      resp = await fetch(url);
-    }
-    else {
-      resp = await fetch(url, {
-        method,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify( body ),
-      });
-    }
-  }
-
-  // this can be done much more cleanly
-  // this can be done much more cleanly
-  // this can be done much more cleanly
-  // this can be done much more cleanly
-  // this can be done much more cleanly
-  // this can be done much more cleanly
-  // this can be done much more cleanly
-  // -simply always apply content-type: application/json, and authorization: token
-  // -then, if we want to send a post request we apend onto the object a body property.
-  // -then, outside the if-else block, we simply do the fetch request with the method, headers, and body
-  // -this assumes we can send Authorization: null, and it will be ignored by the server
-
-  // TODO: PROPER ERROR HANDLING!!!
-  if (!resp.ok) throw new Error('Error thrown in http.js');
+  if (!resp.ok) 
+    throw new Error('Error thrown in http.js');
 
   const data = await resp.json();
 
