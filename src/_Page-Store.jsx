8f540d89e3cf23d19344021baf6e1596f048cf1f
joshdotjs <joshdotjs@gmail.com>
2023-10-10 06:46:49 -0500

    undo album

--- src/_Page-Store.jsx
+++ src/_Page-Store.jsx
@@ -1,32 +1,14 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
+import { useState, useEffect } from 'react';
+import { Container, Typography, Paper, Box, Button } from '@mui/material';
 
-// comps:
 import Layout from './_layout';
 import ProductsGrid from './grid-products';
 import ProductDetailsModal from './modal-product-details';
 
-// utils:
 import { http } from './util/http';
 import { apiUrl } from './util/url';
 import { asynch } from './util/async';
 
-// context:
-
-// hooks:
 import { useNotification } from './hooks/use-notification';
 
 // ==============================================
@@ -34,96 +16,74 @@ import { useNotification } from './hooks/use-notification';
 // ==============================================
 // ==============================================
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+export default function StorePage () {
 
-// ============================================
+  // ============================================
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [products, setProducts] = useState([]);
 
-// ============================================
+  const [notify] = useNotification();
 
-export default function StorePage() {
+  // ============================================
+
+  const [product, setProduct] = useState({
+    uuid: '',
+    title: '',
+    description: '',
+    category: '',
+    status: '',
+    published: false,
+    price: 0,
+    units_in_stock: 0,
+    image_url: '',
+    image_alt: '',
+    details_route: '',
+  });
+
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
+  const openModal = (id) => {
+
+    const product = products.find(product => product.id === id);
+    console.log('product: ', product);
+
+    setProduct(product);
+    setOpen(true);
+  }
+
+
+  // ============================================
+
+  const getProducts = async () => {
+    const URL = apiUrl('products');
+    const promise = http({ url: URL });
+    const [products, error] = await asynch( promise );
+    if (error) {
+      console.error(error);
+      notify({message: 'Error getting users...', variant: 'error', duration: 2000})();
+      return;
+    }
+    console.log('products: ', products);
+    setProducts(products);
+  };
+
+  // ============================================
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  // ============================================
+  
+
   return (
     <Layout>
-      {/* Hero unit */}
-      <Box
-        sx={{
-          bgcolor: 'background.paper',
-          pt: 8,
-          pb: 6,
-        }}
-      >
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="text.primary"
-            gutterBottom
-          >
-            Album layout
-          </Typography>
-          <Typography variant="h5" align="center" color="text.secondary" paragraph>
-            Something short and leading about the collection below—its contents,
-            the creator, etc. Make it short and sweet, but not too short so folks
-            don&apos;t simply skip over it entirely.
-          </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-          >
-            <Button variant="contained">Main call to action</Button>
-            <Button variant="outlined">Secondary action</Button>
-          </Stack>
-        </Container>
-      </Box>
-      <Container sx={{ py: 8 }} maxWidth="md">
-        {/* End hero unit */}
-        <Grid container spacing={4}>
-          {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={4}>
-              <Card
-                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-              >
-                <CardMedia
-                  component="div"
-                  sx={{
-                    // 16:9
-                    pt: '56.25%',
-                  }}
-                  image="https://source.unsplash.com/random?wallpapers"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe the
-                    content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">View</Button>
-                  <Button size="small">Edit</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+
+      <Container sx={{ border: 'solid white 1px', borderTop: 'none', minHeight: '94vh'}}> 
+        <ProductsGrid { ...{ products, openModal } } />
       </Container>
+
+      <ProductDetailsModal {... { open, setOpen, product } } />
     </Layout>
   );
-}
+};
