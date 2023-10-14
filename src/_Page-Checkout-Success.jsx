5ab1bd59e24aa90ad7777fdf0eed315989e40294
joshdotjs <joshdotjs@gmail.com>
2023-10-14 15:00:56 -0500

    map for order summary

--- src/_Page-Checkout-Success.jsx
+++ src/_Page-Checkout-Success.jsx
@@ -12,6 +12,8 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+import CardMedia from '@mui/material/CardMedia';
 
 // ==============================================
 // ==============================================
@@ -37,7 +39,7 @@ const products = [
     desc: 'Best thing of all',
     price: '$14.11',
   },
-  { name: 'Shipping', desc: '', price: 'Free' },
+  { name: 'Tax', desc: '', price: 'TODO' },
 ];
 
 const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
@@ -51,10 +53,13 @@ const payments = [
 function Review() {
   return (
     <>
-      <Typography variant="h6" gutterBottom>
-        Order summary
-      </Typography>
-      <List disablePadding>
+
+      <List
+        disablePadding
+        sx={{
+          mb: '0.5rem',
+        }}
+      >
         {products.map((product) => (
           <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
             <ListItemText primary={product.name} secondary={product.desc} />
@@ -68,13 +73,18 @@ function Review() {
           </Typography>
         </ListItem>
       </List>
-      <Grid container spacing={2}>
+
+
+
+      {/* <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-            Shipping
+            Pickup
           </Typography>
+
           <Typography gutterBottom>John Smith</Typography>
           <Typography gutterBottom>{addresses.join(', ')}</Typography>
+          
         </Grid>
         <Grid item container direction="column" xs={12} sm={6}>
           <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
@@ -93,7 +103,61 @@ function Review() {
             ))}
           </Grid>
         </Grid>
-      </Grid>
+      </Grid> */}
+
+      <Divider
+        sx={{
+          mb: '1rem',
+        }}
+      />
+
+
+      <Box
+        sx={{
+          // background: 'red',
+        }}
+      >
+        <Typography 
+          variant="h6" 
+          sx={{ 
+            mb: '1rem'
+          }}
+        >
+          Pickup
+        </Typography>
+
+        {/* <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9389.833731604926!2d-95.84684155313019!3d36.12365154657924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b6f3250f2af139%3A0xa66db3dab1663301!2sTacos%20Los%20Arellano!5e0!3m2!1sen!2sus!4v1697312837747!5m2!1sen!2sus"
+          // width="600" 
+          // height="450" 
+          style={{ 
+            border: 0,
+            width: '100%',
+            height: '350px',
+            borderRadius: '4px',
+          }} 
+          allowFullScreen="" 
+          loading="lazy" 
+          referrerPolicy="no-referrer-when-downgrade"
+        >
+        </iframe> */}
+
+        <CardMedia
+          component="iframe"
+          // alt={product?.image_alt}
+          // height="140"
+          sx={{ 
+            border: 0,
+            width: '100%',
+            height: '350px',
+            borderRadius: '4px',
+          }}
+          // image={ product?.image_url ?? '/food.jpg' }
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9389.833731604926!2d-95.84684155313019!3d36.12365154657924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b6f3250f2af139%3A0xa66db3dab1663301!2sTacos%20Los%20Arellano!5e0!3m2!1sen!2sus!4v1697312837747!5m2!1sen!2sus"
+        />
+
+      </Box>
+
     </>
   );
 }
@@ -117,7 +181,7 @@ export default function CheckoutSuccess() {
     <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
       <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
         <Typography component="h1" variant="h4" align="center">
-          Checkout
+          Order Summary
         </Typography>
         <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
           {steps.map((label) => (
