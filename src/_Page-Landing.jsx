8a8ec39313046f18e44461562fb292ca0c427324
joshdotjs <joshdotjs@gmail.com>
2023-10-27 14:54:52 -0500

    buttons

--- src/_Page-Landing.jsx
+++ src/_Page-Landing.jsx
@@ -1,7 +1,15 @@
 // libs:
 import * as React from 'react';
 import CardMedia from '@mui/material/CardMedia';
-import level_up from './assets/pizza-wiki.jpeg';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+// imgs:
+import background_img from './assets/pizza-wiki.jpeg';
+import { Typography } from '@mui/material';
 
 // ==============================================
 // ==============================================
@@ -20,12 +28,59 @@ export default function LandingPage () {
         component="img"
         alt="background image"
         height="100%"
-        // sx={{ 
-        //   width: img_size, 
-        //   height: img_size, 
-        // }}
-        image={ level_up }
+        image={ background_img }
       />
+
+      <Backdrop
+        sx={{ 
+          color: '#fff', 
+          zIndex: (theme) => theme.zIndex.drawer + 1 
+        }}
+        open={true}
+        // onClick={handleClose}
+      >
+        {/* <CircularProgress color="inherit" /> */}
+
+        <Box>        
+          <Typography 
+            variant="h2" 
+            sx={{ 
+              color: 'white', 
+              fontSize: '1.3rem',
+            }}>
+            Tulsa, Oklahoma
+          </Typography>
+
+          <Typography 
+            variant="h1" 
+            sx={{
+              color: 'white',
+              mb: 1,
+            }}>
+            Food Truck
+          </Typography>
+
+          <ButtonGroup 
+            variant="contained" 
+            aria-label="outlined primary button group" 
+            sx={{ 
+              display: 'flex',
+              gap: 1,
+              alignItems: 'center',
+            }}
+          >
+            <Button>Order Online</Button>
+            <Button 
+              // variant="outlined" 
+              color='primary'
+              sx={{ flexGrow: 1 }}
+            >
+              Map
+            </Button>
+          </ButtonGroup>
+        </Box>
+
+      </Backdrop>
     </div>
   );
 };
