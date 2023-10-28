5a8fa5c615536ade19aa66c8263dcac772583a7b
joshdotjs <joshdotjs@gmail.com>
2023-10-28 09:43:40 -0500

    remove items

--- src/_Page-Landing-------.jsx
+++ src/_Page-Landing-------.jsx
@@ -56,7 +56,7 @@ const child_variants = {
 // ==============================================
 // ==============================================
 
-export default function LandingPage () {
+export default function Remove () {
 
   // ============================================
 
