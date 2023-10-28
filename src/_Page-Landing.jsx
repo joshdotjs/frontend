4b3430b680bdc91e2203e717bb2a2b4899e6546a
joshdotjs <joshdotjs@gmail.com>
2023-10-28 09:36:19 -0500

    remove items

--- src/_Page-Landing.jsx
+++ src/_Page-Landing.jsx
@@ -1,101 +1,65 @@
-// libs:
-import { useState, useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { motion, AnimatePresence } from 'framer-motion';
-import { Link, NavLink } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import Backdrop from '@mui/material/Backdrop';
-import Container from '@mui/material/Container';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import CircularProgress from '@mui/material/CircularProgress';
-
-// imgs:
-import background_img from './assets/pizza-wiki.jpeg';
+import "./_Page-Landing.css";
+import { useState, useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 // ==============================================
 // ==============================================
 
-const container_variants = {
-  hidden: { // initial
-    opacity: 0, 
-    x: '100vw',
-    transition: {
-      staggerChildren: 0.5,
-    } 
-  },
-  visible: { // animate
-    opacity: 1, 
-    x: 0,
-    transition: { 
-      type: 'spring',
-      mass: 0.4,
-      damping: 8,
-      staggerChildren: 0.4,
-      when: "beforeChildren",
-      delay: 0.5,
-    }
-  },
-};
-
-const child_variants = {
-  hidden: { // initial
-    opacity: 0,
-  },
-  visible: { // animate
-    opacity: 1,
-  }
+function removeItem(arr, item) {
+  const index = arr.indexOf(item);
+  if (index > -1) arr.splice(index, 1);
 }
 
-
 // ==============================================
 // ==============================================
 // ==============================================
 // ==============================================
 
-export default function LandingPage () {
+export default function App() {
+  const count = useRef(0);
+  const [items, setItems] = useState([0]);
+  const [popLayout, setPopLayout] = useState(false);
 
-  // ============================================
-
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [rotate, setRotate] = useState(0);
-
-  const [show_map, setShowMap] = useState(false);
-
-  // ============================================
-  
   return (
-    <div
-      style={{ 
-        height: '100vh',
-        background: 'orange'
-      }}
-    >
-      <Button variant="contained" onClick={() => setShowMap(prev => !prev)}>Click</Button>
-
-      <AnimatePresence>
-        { 
-          show_map && 
-          <motion.div 
-            class="box"
-            exit={{ opacity: 0, height: 0 }}
-          >
-            1
-          </motion.div> 
-        }
-      </AnimatePresence>
-
-      <motion.div 
-        layout 
-        class="box"
-      >
-        2
-      </motion.div>
-      
+    <div className="example">
+      <div className="controls">
+        <label className="enable">
+          <code>popLayout</code>
+          <input
+            type="checkbox"
+            checked={popLayout}
+            onChange={(e) => setPopLayout(e.currentTarget.checked)}
+          />
+        </label>
+        <motion.button
+          whileTap={{ scale: 0.95 }}
+          onClick={() => {
+            count.current++;
+            setItems([...items, count.current]);
+          }}
+        >
+          Add item
+        </motion.button>
+      </div>
+      <ul>
+        <AnimatePresence mode={popLayout ? "popLayout" : "sync"}>
+          {items.map((id) => (
+            <motion.li
+              layout
+              // initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+              transition={{ type: "spring" }}
+              key={id}
+              onClick={() => {
+                const newItems = [...items];
+                removeItem(newItems, id);
+                setItems(newItems);
+              }}
+            />
+          ))}
+        </AnimatePresence>
+      </ul>
     </div>
   );
-};
+}
